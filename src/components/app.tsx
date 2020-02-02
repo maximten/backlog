e5d7bb5e9a2e61826476dd9d4d785edc45cee193
Maxim <maximten94@gmail.com>
2020-02-02 21:47:52 +0300

    styles

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,22 +1,34 @@
 import 'src/styles';
-
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'src/store';
 import { List } from 'src/components/list';
 import { KeyHandler } from 'src/components/key-handler';
 import { Shortcuts } from 'src/components/shortcuts';
 import { Container } from 'src/components/container';
+import styled from 'styled-components';
 
 const store = createStore();
 
-export const App: FC = () => (
-  <Provider store={store}>
-    <Container>
-      <h1>Backlog</h1>
-      <Shortcuts />
-      <List />
-      <KeyHandler />
-    </Container>
-  </Provider>
-);
+const AppBlock = styled.div`
+  opacity: ${(props) => (props.isMounted ? '1' : '0')};
+`;
+
+export const App: FC = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  return (
+    <Provider store={store}>
+      <AppBlock className="app" isMounted={isMounted}>
+        <Container>
+          <h1>Backlog</h1>
+          <Shortcuts />
+          <List />
+          <KeyHandler />
+        </Container>
+      </AppBlock>
+    </Provider>
+  );
+};
